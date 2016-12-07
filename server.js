91455efaf72c185048a386ebeb949788b15edeb9
Vagelis Bismpikis <vagelisbisbikis@gmail.com>
2016-12-07 21:14:17 +0200

    Initialize repo

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+
+var middleware = {
+	requireAuthentication : function (req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function(req, res, next) {
+		console.log('Request: ' +  new Date().toString() + ' ' + req.method + ' ' + req.originalUrl );
+		next();
+		//requests
+		//and urls
+	}
+};
+
+app.use(middleware.logger);
+
+app.get('/about-us', middleware.requireAuthentication, function(req, res) {
+	res.send('About us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.listen(PORT, function () {
+	console.log('Express Server Started on ' + PORT + '!');
+});
