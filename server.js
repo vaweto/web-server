6b69211fae30640955b92b94aca24f96df0d8397
Vagelis Bismpikis <vagelisbisbikis@gmail.com>
2016-12-07 21:15:41 +0200

    Add a small comment

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var middleware = {
 	requireAuthentication : function (req, res, next) {
 		console.log('private route hit!');
 		next();
+		//Private route
 	},
 	logger: function(req, res, next) {
 		console.log('Request: ' +  new Date().toString() + ' ' + req.method + ' ' + req.originalUrl );
